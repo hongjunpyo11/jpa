8045d367e8537041bca57a2d08dfdbc0d987a5e7
hongjunpyo11 <jpyo98@naver.com>
2023-10-25 23:46:44 +0900

    트랜잭션 안에 트랜잭션은 없다. 가장 바깥쪽 트랜잭션만 반영됨

--- src/main/java/com/hjp/db/domain/question/entity/Question.java
+++ src/main/java/com/hjp/db/domain/question/entity/Question.java
@@ -1,25 +1,14 @@
 package com.hjp.db.domain.question.entity;
 
 import com.hjp.db.base.jpa.BaseEntity;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.*;
 
 @Entity
 @Getter
 @SuperBuilder
-@AllArgsConstructor
-@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Question extends BaseEntity {
 
     private String subject;
