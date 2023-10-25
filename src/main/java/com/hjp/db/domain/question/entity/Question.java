07664134984c03cfb2f6f12b7f588c0f548adef1
hongjunpyo11 <jpyo98@naver.com>
2023-10-25 23:35:24 +0900

    인자없는 생성자를 직접 생성시에는 protected 로, @SuperBuilder는 부모와 자식에게 모두 적용해야 한

--- src/main/java/com/hjp/db/domain/question/entity/Question.java
+++ src/main/java/com/hjp/db/domain/question/entity/Question.java
@@ -2,8 +2,11 @@ package com.hjp.db.domain.question.entity;
 
 import com.hjp.db.base.jpa.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,7 +17,9 @@ import static jakarta.persistence.GenerationType.*;
 
 @Entity
 @Getter
-@Builder
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Question extends BaseEntity {
 
     private String subject;
