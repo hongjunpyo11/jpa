a5a2d96fb3db27a68d8f6dc706158312e41998e9
hongjunpyo11 <jpyo98@naver.com>
2023-10-31 21:28:28 +0900

    jpa 만으로 복잡한 쿼리

--- src/main/java/com/hjp/db/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/hjp/db/domain/question/repository/QuestionRepository.java
@@ -1,6 +1,8 @@
 package com.hjp.db.domain.question.repository;
 
 import com.hjp.db.domain.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.List;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     List<Question> findByOrderByIdDesc();
+
+    Page<Question> findDistinctBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswers_contentContainsOrAnswers_author_usernameContains(String kw, String kw1, String kw2, String kw3, String kw4, Pageable pageable);
 }
